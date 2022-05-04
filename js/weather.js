c39802d922b7fc783912eb6e52466b502af735b4
是little颜鸭 <89624700+Dayanshifu@users.noreply.github.com>
2022-05-04 11:31:18 +0800

    Add files via upload

--- js/weather.js
+++ js/weather.js
@@ -2,10 +2,9 @@ WIDGET = {
   "CONFIG": {
     "layout": "1",
     "width": "450",
-    "height": "140",
+    "height": "135",
     "background": "1",
     "dataColor": "FFFFFF",
-    "borderRadius": "15",
-    "key": "a4f4e01400474dac8ac128ea8af978a1"
+    "key": "40364c5e444940918381f2285443e0c8"
   }
 }
