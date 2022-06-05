eb0596bd91dffa59e91e0a164df49ad5b397b1d7
是little颜鸭 <dayanshifu@outlook.com>
2022-06-05 10:29:26 +0800

    .

---
+++ js/mulu.js
@@ -0,0 +1,11 @@
+var closeed = document.getElementById('closedmulu')
+var opened = document.getElementById('openedmulu')
+function muluon(){
+    opened.style.display="block"
+    closeed.style.display="none"
+}
+function muluoff(){
+    opened.style.display="none"
+    closeed.style.display="block"
+}
+muluoff()
