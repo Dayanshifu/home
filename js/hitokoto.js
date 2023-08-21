dc36c54c878002d996147bcbaa5d4eb9becb1a1a
是little颜鸭 <dayanshifu@outlook.com>
2023-08-22 00:17:09 +0800

    6

---
+++ js/hitokoto.js
@@ -0,0 +1,15 @@
+var p = document.getElementById("hitokoto");
+    var q = document.getElementById("fromwho");
+
+    // 发起请求
+    fetch("https://v1.hitokoto.cn/?c=i&c=g&c=f")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            // 将hitokoto内容替换到p标签中
+            p.innerText = "「  "+ data.hitokoto+"」";
+            if(data.from_who!=null){
+            q.innerText =data.from_who+" "+data.from;}
+            else{q.innerText =data.from;}
+        });
