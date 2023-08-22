d4c963b61224692603119a9f6dd0787f599b292e
是little颜鸭 <dayanshifu@outlook.com>
2023-08-22 12:30:21 +0800

    6

--- js/hitokoto.js
+++ js/hitokoto.js
@@ -1,15 +1,22 @@
 var p = document.getElementById("hitokoto");
-    var q = document.getElementById("fromwho");
-
-    // 发起请求
-    fetch("https://v1.hitokoto.cn/?c=i&c=g&c=f")
+var q = document.getElementById("fromwho");
+// 发起请求
+function getHitokoto(){
+    fetch("https://v1.hitokoto.cn/?c=i&c=c")
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
-            // 将hitokoto内容替换到p标签中
-            p.innerText = "「  "+ data.hitokoto+"」";
-            if(data.from_who!=null){
-            q.innerText =data.from_who+" "+data.from;}
-            else{q.innerText =data.from;}
+            if((data.type=='c'&& (data.from=="原神" || data.from_who=="原神"))||data.type!='c'){
+                // 将hitokoto内容替换到p标签中
+                p.innerText = "「  "+ data.hitokoto+"」";
+                if(data.from_who!=null){
+                q.innerText ='——'+data.from_who/*+" "+data.from*/;}
+                else{
+                    q.innerText=data.from
+                }
+            }else{getHitokoto()}
         });
+    }
+
+getHitokoto();
