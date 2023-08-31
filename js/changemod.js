601330df95a4e7ea1cdd4338de1af1e647280cae
是little颜鸭 <dayanshifu@outlook.com>
2023-08-31 22:02:08 +0800

    修复

--- js/changemod.js
+++ js/changemod.js
@@ -3,14 +3,14 @@ var sites = document.getElementById('sites');
 var wordl = document.getElementById('word')
 
 function changemod(){
-  if (sites.style.display === 'none') {  
+  if (sites.style.display == 'none') {  
     sites.style.display = 'block';  
     wg.style.display = 'none';  
-    localStorage.setItem("mod","wg")
+    localStorage.setItem("mod","sites")
   } else {  
     sites.style.display = 'none';  
     wg.style.display = 'block';  
-    localStorage.setItem("mod","sites")
+    localStorage.setItem("mod","wg")
   }  
 }
 
