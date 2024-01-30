238018a71449e6a1ec95f87d6946c0630cc877cb
little颜 <dayanshifu@outlook.com>
2024-01-30 12:11:24 +0800

    Update changetime.js

--- js/changetime.js
+++ js/changetime.js
@@ -1,20 +1,19 @@
 
 var nowSeconds=document.getElementById("nowSeconds")
 var swit=document.getElementById("switch")
+
 if(localStorage.getItem("time")==0){
-    nowSeconds.style.display='inline'
-    swit.checked=true
-    localStorage.setItem("time",1)
-} 
-else if(localStorage.getItem("time")==1){
     nowSeconds.style.display='none'
     swit.checked=false
-    localStorage.setItem("time",0)
-}
-else{
+} 
+else if(localStorage.getItem("time")==1){
     nowSeconds.style.display='inline'
     swit.checked=true
-    localStorage.setItem("time",1)   
+}
+else{
+    nowSeconds.style.display='none'
+    swit.checked=false
+    localStorage.setItem("time",0)   
 } 
 function changetime(){
     if(localStorage.getItem("time")==0){
@@ -28,8 +27,8 @@ function changetime(){
         localStorage.setItem("time",0)
     }
     else{
-        nowSeconds.style.display='inline'
-        swit.checked=true
-        localStorage.setItem("time",1)   
+        nowSeconds.style.display='none'
+        swit.checked=false
+        localStorage.setItem("time",0)   
     } 
 }
