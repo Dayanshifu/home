be342b81941c46afafeefdc42502ae584d084e1b
little颜 <dayanshifu@outlook.com>
2024-03-10 12:07:58 +0800

    Update connection.js

--- js/connection.js
+++ js/connection.js
@@ -93,7 +93,7 @@ window.addEventListener('resize', function() {
     else if (isgpt) {
         setTimeout(function(){
             div.scrollTop = div.scrollHeight;
-        },400 ) 
+        },300 ) 
         isgpt = false
     }        
     });
